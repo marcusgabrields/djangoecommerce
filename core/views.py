d2d12f43f5fd3001dbbda3e47f4d08bb1d8f02d7
Marcus Gabriel Dantas Salgues <marcusgabriel.ds@gmail.com>
2018-10-18 22:23:29 -0300

    Add some views and templates

--- core/views.py
+++ core/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        'title': 'django e-commerce'
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+def product(request):
+    return render(request, 'product.html')
