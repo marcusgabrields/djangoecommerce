09e06ae28d59dc10dcb443e11a4b0c77a3f2b5ec
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2018-10-29 14:43:56 -0300

    Refactoring the views to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Category, Product
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category), 
-    }
-    return render(request, 'catalog/category.html', context)
+    model = Product
+    template_name = 'catalog/product_list.html'
+
+
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+    
+
+
 
 def product(request, slug):
     product = Product.objects.get(slug=slug)
@@ -23,3 +33,8 @@ def product(request, slug):
         'product': product,
     }
     return render(request, 'catalog/product.html', context)
+
+
+product_list = ProductListView.as_view()
+
+category = CategoryListView.as_view()
